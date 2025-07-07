24b1fe6dd6380dac198159334d5e61d7c1b76030
Manvir Odedra <manvirodedara@gmail.com>
2025-07-07 17:53:13 +0530

    remove model

--- foodwala/home/admin.py
+++ foodwala/home/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from home.models import *
 
 # Register your models here.
-admin.site.register(Contact)
+
 admin.site.register(Category)
 admin.site.register(Food)
 admin.site.register(CartItem)
