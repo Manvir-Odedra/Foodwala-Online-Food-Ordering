7a9bc37089a0bf5adeee7354619e5c5735a4c000
Manvir Odedra <manvirodedara@gmail.com>
2025-07-07 18:20:51 +0530

    Modify

--- foodwala/home/models.py
+++ foodwala/home/models.py
@@ -1,17 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from foodwala.models import *
 # Create your models here.
-class Contact(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    number = models.IntegerField()
-    subject = models.CharField(max_length=100)
-    message = models.TextField()
 
-    def __str__(self):
-        return self.name
-    
 class Category(models.Model):
     cat_name = models.CharField(max_length=100)
     cat_img = models.ImageField(upload_to='catimg/')
