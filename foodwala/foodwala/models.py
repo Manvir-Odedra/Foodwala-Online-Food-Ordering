51bf488ae70cd1bbffe9282e0e2c24657fa3e662
Manvir Odedra <manvirodedara@gmail.com>
2025-07-07 17:14:28 +0530

    Add models.py files

---
+++ foodwala/foodwala/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+import uuid
+
+class BaseModel(models.Model):
+    uid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, )
+    creatred_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
