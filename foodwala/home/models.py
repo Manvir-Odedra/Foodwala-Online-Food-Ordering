08e7baeecd44a4ed0e2231bff807022f68fdc0b0
Manvir Odedra <manvirodedara@gmail.com>
2025-07-08 06:59:28 +0530

    Deleted Model

--- foodwala/home/models.py
+++ foodwala/home/models.py
@@ -3,17 +3,7 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-class Contact(models.Model):
 
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    number = models.IntegerField()
-    subject = models.CharField(max_length=100)
-    message = models.TextField()
-
-    def __str__(self):
-        return self.name
-    
 
 class Category(models.Model):
 
