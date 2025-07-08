8612d6c95bb610d0482a8ee93e63c90060a99d21
Manvir Odedra <manvirodedara@gmail.com>
2025-07-08 15:54:38 +0530

    Modify Admin

--- foodwala/home/admin.py
+++ foodwala/home/admin.py
@@ -3,6 +3,13 @@ from home.models import *
 
 # Register your models here.
 
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'number', 'subject', 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')  # Optional: prevent editing
+
+    # Optional: show fields in detail view
+    fields = ('name', 'email', 'number', 'subject', 'message', 'created_at', 'updated_at')
 admin.site.register(Category)
 admin.site.register(Food)
 admin.site.register(CartItem)
