d32c8627408f068a4da087481e33d7735e0579be
Manvir Odedra <manvirodedara@gmail.com>
2025-07-08 07:49:06 +0530

    Create models file

--- foodwala/home/models.py
+++ foodwala/home/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from foodwala.models import BaseModel
+from base.models import BaseModel
 
 # Create your models here.
 class Contact(BaseModel):
