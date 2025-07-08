158facf92cff2884d5fbd32eef5008d4df7248a1
Manvir Odedra <manvirodedara@gmail.com>
2025-07-08 06:55:59 +0530

    Modify Files

--- foodwala/home/models.py
+++ foodwala/home/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-from foodwala.models import *
+
 
 # Create your models here.
-class Contact(BasseModel):
+class Contact(models.Model):
 
     name = models.CharField(max_length=100)
     email = models.EmailField()
